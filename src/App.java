974ce1fbd6143619007ff997ff793ac829ca35c0
Andrey Fialho <fialho.andrey2017@gmail.com>
2022-03-07 09:31:03 -0300

    Criação da classe Position

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
+import board.Position;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Position p = new Position(1, 2);
+
+        System.out.println(p);
     }
 }
