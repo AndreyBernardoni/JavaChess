60d9de0a860d3e8e809e4a63d475d2044233bd5f
Andrey Fialho <fialho.andrey2017@gmail.com>
2022-03-08 14:04:08 -0300

    Implementação da board

--- src/App.java
+++ src/App.java
@@ -1,9 +1,9 @@
-import board.Position;
+import chess.ChessMatch;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Position p = new Position(1, 2);
 
-        System.out.println(p);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
